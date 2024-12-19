731f9c4e782d4161dddbca90a417d686e09d5df2
garinyanjoy <garinyanjoy@gmail.com>
2024-12-20 01:29:40 +0800

    更新dockerfile，支持vue热更新，解决跨域数据传输问题

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,4 +1,21 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    host: '0.0.0.0',
+    port: 8080,
+    hot: true,
+    client: {
+      // 修改 WebSocket 配置
+      webSocketURL: 'auto://0.0.0.0:0/ws'
+    },
+    watchFiles: {
+      paths: ['src/**/*.*'],
+      options: {
+        usePolling: true,
+        interval: 1000,
+        ignored: /node_modules/
+      }
+    }
+  }
 })
